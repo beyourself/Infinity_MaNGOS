a69aff409dc97e4fcaec087a66f071c8614010b4
fallenangelX <noneya@hotmail.com>
2014-03-14 03:29:25 -0400

    * HoS -> sd2
    Signed-off-by: fallenangelX <noneya@hotmail.com>

--- sql/Development/I_MaNGOS_sd2/mr00585_mangos_halls_of_stone.sql
+++
@@ -1,2 +0,0 @@
-UPDATE `creature_template` SET `ScriptName` = 'boss_krystallus' WHERE `entry` = 27977;
-DELETE FROM `achievement_criteria_requirement` WHERE `criteria_id` = 7143 AND `type` = 18; -- Achievement 1866
