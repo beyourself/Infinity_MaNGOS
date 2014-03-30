2a115d8e10dc52fbce8a36d6acce6883400ff24b
ConanHUN <conan_513@hotmail.com>
2014-03-30 16:01:51 +0200

    Missing files

---
+++ src/shared/revision.h
@@ -0,0 +1,6 @@
+#ifndef __REVISION_H__
+#define __REVISION_H__
+ #define REVISION_ID "2fe7e8bed488b6fe5cc1e831b7f58180def6f71e"
+ #define REVISION_DATE "2014-03-29"
+ #define REVISION_TIME "12:23:33"
+#endif // __REVISION_H__
